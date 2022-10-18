63200eaaa79826e3b20d76f6334859efcdc0ff0a
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-18 15:06:49 -0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
 
 function App() {
+  type CategoryType = {
+    id: number
+    title: string
+  }
+
+  const categories: CategoryType[] = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ]
+
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
+    <div className='categories-container'>
+      {categories.map(({ title, id }) => (
+        <div key={id} className='category-container'>
+          <div className='background-image' />
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
