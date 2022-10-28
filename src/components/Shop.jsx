9a190c1c3c77534dad43594edc709a1136d16edb
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-28 15:55:17 -0700

    shop page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,60 +1,59 @@
-import { useState, useContext, useEffect } from 'react'
-import { CategoriesContext } from '../context/CategoriesContext'
-import ProductCard from './ProductCard'
+import { Link } from 'react-router-dom'
 
 const shop = () => {
-  const { categories } = useContext(CategoriesContext)
-
-  const CategoryPreview = (categoryTitle, limit) => {
-    const products = categories[categoryTitle].map((item) => (
-      <div key={item.id} className='group relative'>
-        <div className='relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1'>
-          <img
-            src={item.imageUrl}
-            alt={item.title}
-            className='h-full w-full object-cover object-center'
-          />
-        </div>
-        <h3 className='mt-6 text-sm text-gray-500'>
-          <a href={'#'}>
-            <span className='absolute inset-0' />
-            {item.title}
-          </a>
-        </h3>
-        <p className='text-base font-semibold text-gray-900'>{item.title}</p>
-      </div>
-    ))
-
-    return (
-      <div className='bg-gray-100'>
-        <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-          <div className='mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32'>
-            <h2 className='text-2xl font-bold text-gray-900'>{categoryTitle}</h2>
-
-            <div className='mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0'>
-              {products.slice(0, limit)}
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  // const ProductsForACategory = (categoryTitle, limit) => {
-  //   const products = categories[categoryTitle].map((item) => {
-  //     return <ProductCard key={item.id} product={item} />
-  //   })
-
-  //   return products.slice(0, limit)
-  // }
+  const categoryNames = [
+    {
+      name: 'Sneaksers',
+      description: 'Sneakers for all occasions',
+      imageSrc: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+      imageAlt:
+        'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+      href: '#',
+    },
+    {
+      name: 'Jackets',
+      description: 'Rain or shine, we have you covered',
+      imageSrc: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+      imageAlt:
+        'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+      href: '#',
+    },
+    {
+      name: 'Mens',
+      description: 'Nothing but the best for the fellas',
+      imageSrc: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+      href: '#',
+    },
+  ]
 
   return (
-    <>
-      {CategoryPreview('jackets', 3)}
-      {CategoryPreview('sneakers', 3)}
-      {CategoryPreview('womens', 3)}
-      {CategoryPreview('mens', 3)}
-    </>
+    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+      <div className='mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32'>
+        <h2 className='text-2xl font-bold text-gray-900'>Collections</h2>
+
+        <div className='mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0'>
+          {categoryNames.map((item) => (
+            <div key={item.id} className='group relative'>
+              <div className='relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1'>
+                <img
+                  src={item.imageSrc}
+                  alt={item.imageAlt}
+                  className='h-full w-full object-cover object-center'
+                />
+              </div>
+              <h3 className='mt-6 text-sm text-gray-500'>
+                <Link to={`/shop/${item.name.toLowerCase()}`}>
+                  <span className='absolute inset-0' />
+                  {item.name}
+                </Link>
+              </h3>
+              <p className='text-base font-semibold text-gray-900'>{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
