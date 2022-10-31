5206b6132e6d5dbb156cb5454a0301577e6c6bc7
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-31 14:34:30 -0700

    reducer for user

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useEffect, useReducer } from 'react'
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -10,10 +10,47 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 })
 
+export const USER_ACTION_TYPES = {
+  SET_USER: 'SET_USER',
+}
+
+// create a reducer function to update the state
+const userReducer = (state, action) => {
+  // we always have a type and a payload on the action
+  const { type, payload } = action
+
+  // use a switch statement to handle different actions
+  switch (type) {
+    case 'SET_USER':
+      return {
+        ...state, // spread the current state
+        currentUser: payload,
+      }
+    default:
+      throw new Error(`Unhandled action type: ${type}`)
+  }
+}
+
+const INITIAL_USER_STATE = {
+  currentUser: null,
+}
+
 // provider component
 export const UserProvider = ({ children }) => {
+  // use the reducer to update the state
+  const [state, dispatch] = useReducer(userReducer, INITIAL_USER_STATE)
+
+  const { currentUser } = state
+
+  const setCurrentUser = (user) => {
+    dispatch({
+      type: USER_ACTION_TYPES.SET_USER,
+      payload: user,
+    })
+  }
+
   // the initial state of the user context, base, empty state
-  const [currentUser, setCurrentUser] = useState(null)
+  // const [currentUser, setCurrentUser] = useState(null)
   // allows you to pass the getter and setter functions to the children components
   const value = { currentUser, setCurrentUser }
 
@@ -30,3 +67,9 @@ export const UserProvider = ({ children }) => {
   }, [])
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
+
+// const userReducer = (state, action) => {
+//   return {
+//     currentUser: null,
+//   }
+// }
