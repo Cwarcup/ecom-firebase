cc84d488c0942f5ee828be04abce6096b1445f1b
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-11-01 11:55:30 -0700

    initial reducer setup

--- src/index.jsx
+++ src/index.jsx
@@ -5,19 +5,26 @@ import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './context/UserContext'
 import { CategoriesProvider } from './context/CategoriesContext'
 import { CartProvider } from './context/CartContext'
+import { Provider } from 'react-redux'
+import React from 'react'
+import { store } from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 // must wrap App in BrowserRouter to use react-router-dom
 
 root.render(
-  <BrowserRouter>
-    <UserProvider>
-      <CategoriesProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </CategoriesProvider>
-    </UserProvider>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,10 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import { rootReducer } from './rootReducer'
+
+// middleware
+const middlewares = [logger]
+const composedEnhancers = compose(applyMiddleware(...middlewares))
 
 // root reducer
+export const store = createStore(rootReducer, undefined, composedEnhancers)
